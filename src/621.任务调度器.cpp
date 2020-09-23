bc6124d58c8d9d148e277c2dc1e4cc974d44ca27
Bohaixileiyn <shockerfi@foxmail.com>
2020-09-23 21:58:22 +0800

    9.23

---
+++ src/621.任务调度器.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+//暴力递归解法
+// 执行用时：156 ms, 在所有 C++ 提交中击败了50.35% 的用户
+// 内存消耗：35.6 MB, 在所有 C++ 提交中击败了11.13% 的用户
+//需要考虑n为0及freq全为0时的特殊情况
+int pure(vector<int> freq, int n){
+    sort(freq.begin(), freq.end(), greater<int>());
+    int no = 0;
+    for(auto &c: freq){
+        if(c)no++;
+    }
+    if(!no)return 0;
+    if(no < n + 1){
+        int m = freq[0];
+        int maxno = 0;
+        for(unsigned i = 0; i < freq.size(); i++){
+            if(freq[i] == m)maxno++;
+            else break;
+        }
+        return (n + 1) * (m - 1) + maxno;
+    }else{
+        for(int i = 0; i < n + 1; i++){
+            freq[i]--;
+        }
+        return n + 1 + pure(freq, n);
+    }
+}
+
+int BFleastInterval(vector<char>& tasks, int n) {
+    if(!n)return tasks.size();
+    vector<int> freq(26, 0);
+    for(auto &c: tasks){
+        freq[c - 'A']++;
+    }
+    return pure(freq, n);
+}
+
+int leastInterval(vector<char>& tasks, int n) {
+    vector<int> cnt(26, 0);
+    for (char task : tasks) {
+        ++cnt[task - 'A'];
+    }
+    sort(cnt.begin(), cnt.end());
+    int i = 25, mx = cnt[25], len = tasks.size();
+    while (i >= 0 && cnt[i] == mx) --i;
+    return max(len, (mx - 1) * (n + 1) + 25 - i);
+}
+
+int main(){
+    vector<char> tasks{'A', 'B', 'C', 'D', 'E', 'A', 'B', 'C', 'D', 'E'};
+    int n = 4;
+    cout << leastInterval(tasks, n) << endl;
+}
--- src/97交错字符串.cpp
+++ src/97交错字符串.cpp
@@ -11,27 +11,31 @@ bool BFisInterleave(string s1, string s2, string s3) {
     else if(s2[0] == s3[0])return BFisInterleave(s1, s2.substr(1), s3.substr(1));
     else return false;
 }
-
+//动态规划
+// 执行用时：0 ms, 在所有 C++ 提交中击败了100.00% 的用户
+// 内存消耗：5.9 MB, 在所有 C++ 提交中击败了99.80% 的用户
 bool isInterleave(string s1, string s2, string s3) {
-    // if(s1.size() + s2.size() != s3.size())return false;
-    // if(!s3.size())return !s1.size() && !s2.size();
-    // else if(!s2.size())return s1 == s3;
-    // else if(!s1.size())return s2 == s3;
-    // //限定s3长度是s1和s2的长度和
-    // bool dp[s1.size()][s2.size()];
-    // dp[0][0] = true;
-    // for(unsigned i = 1; i < s1.size(); i++){
-    //     dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
-    // }
-    // for(unsigned i = 1; i < s2.size(); i++){
-    //     dp[0][i] = dp[0][i - 1] && s2[i - 1] == s3[i - 1];
-    // }
-    // for(unsigned i = 1; i < s1.size(); i++){
-    //     for(unsigned j = 1; j < s2.size(); j++){
-    //         dp[i][j] = (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1])||(dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]);
-    //     }
-    // }
-    // return dp[s1.size() - 1][s2.size() - 1];
+    if(s1.size() + s2.size() != s3.size())return false;
+    if(!s3.size())return !s1.size() && !s2.size();
+    else if(!s2.size())return s1 == s3;
+    else if(!s1.size())return s2 == s3;
+    //限定s3长度是s1和s2的长度和
+    bool dp[s1.size() + 1][s2.size() + 1];
+    dp[0][0] = true;
+
+    for(unsigned i = 1; i <= s1.size(); i++){
+        dp[i][0] = dp[i - 1][0] && s1[i - 1] == s3[i - 1];
+    }
+    for(unsigned i = 1; i <= s2.size(); i++){
+        dp[0][i] = dp[0][i - 1] && s2[i - 1] == s3[i - 1];
+    }
+    
+    for(unsigned i = 1; i <= s1.size(); i++){
+        for(unsigned j = 1; j <= s2.size(); j++){
+            dp[i][j] = (dp[i][j - 1] && s2[j - 1] == s3[i + j - 1])||(dp[i - 1][j] && s1[i - 1] == s3[i + j - 1]);
+        }
+    }
+    return dp[s1.size()][s2.size()];
 }
 
 int main(){
