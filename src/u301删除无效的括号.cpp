237a198299a1c15e2a32722a3d643cf5b7745612
Bohaixileiyin <shockerfi@foxmail.com>
2021-01-25 23:28:13 +0800

    2021.1.25

--- src/u301删除无效的括号.cpp
+++ src/u301删除无效的括号.cpp
@@ -5,7 +5,7 @@ using namespace std;
 // 内存消耗：17 MB, 在所有 C++ 提交中击败了29.01% 的用户
 bool isValid(string t) {
     int cnt = 0;
-    for (int i = 0; i < t.size(); ++i) {
+    for (int i = 0; i < (int)t.size(); ++i) {
         if (t[i] == '(') ++cnt;
         else if (t[i] == ')' && --cnt < 0) return false;
     }
@@ -14,8 +14,9 @@ bool isValid(string t) {
 //bfs
 vector<string> removeInvalidParentheses(string s) {
     vector<string> res;
-    unordered_set<string> visited{{s}};
-    queue<string> q{{s}};
+    unordered_set<string> visited({s});
+    queue<string> q({s});//这里不能写成q{s}会编译错误   
+    //因为初始化容器 需要用的是一个列表的的值例如{s, ..., }   上面同理，也许有些容器可以不加小括号，但为了保险起见，以后初始化一律写成({...})的形式
     bool found = false;
     while (!q.empty()) {
         string t = q.front(); q.pop();
@@ -24,7 +25,7 @@ vector<string> removeInvalidParentheses(string s) {
             found = true;
         }
         if (found) continue;
-        for (int i = 0; i < t.size(); ++i) {
+        for (int i = 0; i < (int)t.size(); ++i) {
             if (t[i] != '(' && t[i] != ')') continue;
             string str = t.substr(0, i) + t.substr(i + 1);
             if (!visited.count(str)) {
@@ -38,4 +39,5 @@ vector<string> removeInvalidParentheses(string s) {
 
 int main(){
     cout << "print something." << endl;
+    cout << __cplusplus << endl;
 }
