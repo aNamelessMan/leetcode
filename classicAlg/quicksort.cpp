6158ebcb88697ad8bf1d80b294e11e9b9ef036c3
Bohaixileiyin <shockerfi@foxmail.com>
2021-02-15 00:11:46 +0800

    2021.2.14

--- classicAlg/quicksort.cpp
+++ classicAlg/quicksort.cpp
@@ -20,6 +20,12 @@ int partition(std::vector<int> &A, int l, int h){
 void quicksort(std::vector<int> &A, int l, int h){
     if(l < h){
         int p = partition(A, l, h);//将A[h]放到正确的位置p，且p之前的元素都小于A[p]，p之后的元素都大于等于A[p]，因此只需再把前后子数组排序完成即可
+        /*!!!
+        此外因为这一步将A[p]元素放到了合适的位置p，所以如果需要找数组中第k大的数，可以根据p与k的大小关系缩小查找范围
+            p < k   return quicksort(A, p + 1, h);  必在子数组A[p + 1, h]中
+            p == k  return A[p];
+            p > k   return quicksort(A, l, p - 1);  必在子数组A[l, p - 1]中
+        */
         quicksort(A, l, p - 1);
         quicksort(A, p + 1, h);
     }
