01025dda3b1eea5dff8ceb078998f5002fce94f0
Bohaixileiyin <shockerfi@foxmail.com>
2021-02-23 18:12:48 +0800

    2021.2.23

--- bytedance7days/LC146.LRU缓存机制.cpp
+++ bytedance7days/LC146.LRU缓存机制.cpp
@@ -1,15 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// 执行用时：108 ms, 在所有 C++ 提交中击败了76.71% 的用户
-// 内存消耗：41.1 MB, 在所有 C++ 提交中击败了21.24% 的用户
+// 执行用时：100 ms, 在所有 C++ 提交中击败了88.02% 的用户
+// 内存消耗：41.2 MB, 在所有 C++ 提交中击败了19.91% 的用户
 //2021.2.23日二刷
 class LRUCache {
 private:
     int cap;
     list<pair<int,int>> l;
     unordered_map<int, list<pair<int,int>>::iterator> m;//!!!!!!!!!!!!  关键是想到map中保存的是key对应的list中的位置，这样才能实现O(1)复杂度的操作
-public:
     LRUCache(int capacity) {
         cap = capacity;
     }
@@ -32,7 +31,8 @@ public:
             l.erase(iter);
         }else{
             if(m.size() == cap){
-                auto iter = l.rbegin();//注意此处不能去end()再--来获取最后一个元素值，因为list迭代器是前向类型，只能取rbegin()
+                auto iter = l.end();
+                iter--;
                 auto k = iter->first;
                 l.erase(m[k]);
                 m.erase(m.find(k));
