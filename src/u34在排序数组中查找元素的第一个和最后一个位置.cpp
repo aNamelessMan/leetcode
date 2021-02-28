742865a267f1000fa7dad1b5f44225bf8318264e
Bohaixileiyin <shockerfi@foxmail.com>
2021-02-28 20:08:37 +0800

    2021.2.28

--- src/u34在排序数组中查找元素的第一个和最后一个位置.cpp
+++ src/u34在排序数组中查找元素的第一个和最后一个位置.cpp
@@ -1,6 +1,47 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// 执行用时：4 ms, 在所有 C++ 提交中击败了99.61% 的用户
+// 内存消耗：13.3 MB, 在所有 C++ 提交中击败了90.91% 的用户
+//看的别人的解法不太懂，还不如自己的解法，虽然繁琐，但是简单
+int lbs(vector<int>& nums, int target, int l, int r){
+    while(l < r){
+        int mid = (l + r) / 2;
+        if(nums[mid] < target){
+            l = mid + 1;
+        }else if(nums[mid] == target){
+            r = mid;
+        }else{
+            r = mid - 1;
+        }
+    }
+    return l;
+}
+int rbs(vector<int>& nums, int target, int l, int r){
+    while(l < r){
+        int mid = (l + r + 1) / 2;
+        if(nums[mid] > target){
+            r = mid - 1;
+        }else if(nums[mid] == target){
+            l = mid;
+        }else{
+            l = mid + 1;
+        }
+    }
+    return l;
+}
+
+vector<int> searchRange(vector<int>& nums, int target) {
+    int l = 0, r = nums.size() - 1;
+    vector<int> res(2, -1);
+    if(r < 0)return res;
+    int lb = lbs(nums, target, l, r);
+    int rb = rbs(nums, target, l, r);
+    res[0] = nums[lb] != target?-1:lb;
+    res[1] = res[0] == -1 || nums[rb] != target?-1:rb;
+    return res;
+}
+
 // 执行用时：12 ms, 在所有 C++ 提交中击败了98.53% 的用户
 // 内存消耗：13.7 MB, 在所有 C++ 提交中击败了18.79% 的用户
 //写的很繁琐，但是思路很简单
@@ -54,7 +95,7 @@ int lbs(int l, int h, int target, vector<int>& nums){
     }
 }
 
-vector<int> searchRange(vector<int>& nums, int target) {
+vector<int> firstsearchRange(vector<int>& nums, int target) {
     int n = nums.size();
     if(n == 0)return {-1, -1};
     return {lbs(0, n - 1, target, nums), rbs(0, n - 1, target, nums)};
@@ -86,5 +127,7 @@ vector<int> PresearchRange(vector<int>& nums, int target) {
 int main(){
     vector<int> nums = {5,7,7,8,8,10};
     vector<int> res = searchRange(nums, 8);
+    nums = {2, 2};
+    res = searchRange(nums, 3);
     cout << 1 << endl;
 }
