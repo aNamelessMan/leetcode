3357b36c3c893261c7fe910bc85b46faf03e0e4f
Bohaixileiyin <shockerfi@foxmail.com>
2021-06-11 00:01:42 +0800

    2021.610

--- src/647回文子串.cpp
+++ src/647回文子串.cpp
@@ -1,5 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+// 执行用时：0 ms, 在所有 C++ 提交中击败了100.00% 的用户
+// 内存消耗：6.2 MB, 在所有 C++ 提交中击败了65.17% 的用户
+//  2021.6.10   暴力解法
+int countSubstrings(string s) {
+    int res = 0, n = s.size(), i = 0;
+    while(i < n){
+        //i作为对称轴
+        int l = i, r = i;
+        while(l >= 0 && r < n && s[l] == s[r]){
+            l--;
+            r++;
+            res++;
+        }
+        //i 和 i+1 作为对称轴
+        l = i, r = i + 1;
+        while(l >= 0 && r < n && s[l] == s[r]){
+            l--;
+            r++;
+            res++;
+        }
+        i++;
+    }
+    return res;
+}
+
 //动态规划
 // 执行用时：24 ms, 在所有 C++ 提交中击败了39.31% 的用户
 // 内存消耗：10.5 MB, 在所有 C++ 提交中击败了22.57% 的用户
