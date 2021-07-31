b0d262e7b73b546951ab64d691884ad8bf80e204
Bohaixileiyin <shockerfi@foxmail.com>
2021-07-31 18:48:17 +0800

    2021.7.31

--- nowcodersql/3.sql
+++ nowcodersql/3.sql
@@ -1,3 +1,3 @@
 select salaries.*, dept_no from salaries, dept_manager where salaries.emp_no = dept_manager.emp_no
-/*选择要展示的行            从哪两个表                   联结(join)条件*/
+/*选择要展示的列            从哪两个表                   联结(join)条件*/
 select salaries.*, dept_manager.dept_no from salaries inner join dept_manager on salaries.emp_no = dept_manager.emp_no
