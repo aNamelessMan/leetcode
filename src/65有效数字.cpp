f7b4e20853c05b8c9e8b2758aeb8bd80cac7915a
A_man <qinjuncc@qq.com>
2023-06-27 22:53:30 +0800

    20230627

--- src/65有效数字.cpp
+++ src/65有效数字.cpp
@@ -1,6 +1,90 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// 20230627 这个写法很清晰，按E/e划分，然后分别判断前后是否符合小数/整数
+class Solution {
+public:
+    bool isExactNo(string s) {
+        if(s.size() == 0)return false;
+        int i = 0;
+        if(s[i] == '-' || s[i] == '+'){
+            i++;
+        }
+        if(i >= s.size())return false;
+        while(i < s.size()) {
+            if(s[i] > '9' || s[i] < '0')return false;
+            i++;
+        }
+        return true;
+    }
+    bool isSmallNo(string s) {
+        if(s.size() == 0)return false;
+        int i = 0;
+        if(s[i] == '-' || s[i] == '+'){
+            i++;
+        }
+        if(i >= s.size())return false;
+
+        if(s.substr(i) == ".")return false;
+        bool allowDot = true;
+        while(i < s.size()) {
+            if(s[i] > '9' || s[i] < '0') {
+                if(s[i] == '.'){
+                    if(allowDot){
+                        allowDot = false;
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }
+            i++;
+        }
+        return true;
+    }
+    bool isNumber(string s) {
+        int n = s.size();
+        int b = -1, e = -1;
+        for(int i = 0; i < n; i++)
+        {
+            if(s.at(i) != ' ')
+            {
+                b = i;
+                break;
+            }
+        }
+        if(b == -1) return false;
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(s.at(i) != ' ')
+            {
+                e = i;
+                break;
+            }
+        }
+        int size = e - b + 1;
+        string v = s.substr(b, size);
+        
+        bool hasE = false;
+        int eIndex = -1, i = 0;
+        while(i < size) {
+            if(v[i] == 'e' || v[i] == 'E') {
+                hasE = true;
+                eIndex = i;
+                break;
+            }
+            i++;
+        }
+        if(hasE){
+            return (isExactNo(v.substr(0, i)) || isSmallNo(v.substr(0, i))) && isExactNo(v.substr(i + 1));
+        }else{
+            return isExactNo(v) || isSmallNo(v);
+        }
+
+    }
+};
+
 //简单解法见剑指offer第20题
 // 执行用时：0 ms, 在所有 C++ 提交中击败了100.00% 的用户
 // 内存消耗：6.4 MB, 在所有 C++ 提交中击败了20.27% 的用户
